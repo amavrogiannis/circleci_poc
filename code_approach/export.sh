c7729293fe25d35f2b211f4500823b6d5bc11b7a
Alex Mavrogiannis <alex.mavrogiannis@gmail.com>
2023-07-03 11:34:07 +0100

    rm secrets

--- code_approach/export.sh
+++
@@ -1,7 +0,0 @@
-CIRCLECI_TOKEN="CCIPAT_EtPfFC6DMgP2Dnk1e4enLC_4865ed8632b9ff95a4b16c84c7c3d8b2159530f2"
-VCS_TYPE=github
-USERNAME=amavrogiannis
-PROJECT=circleci_poc
-
-curl -s "https://circleci.com/api/v2/project/$VCS_TYPE/$USERNAME/$PROJECT/pipeline?circle-token=$CIRCLECI_TOKEN" | jq '.' > circleci_jobs.json
-
